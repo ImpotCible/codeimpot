00c6a4cbca1f56aac47230200b041ef3a94a1116
Jason BOURLARD <jason.bourlard@soprasteria.com>
2016-04-02 15:19:06 +0200

    Modification du footer.scss

--- gulpfile.js
+++ gulpfile.js
@@ -8,27 +8,9 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./src/main/webapp/css'))
 });
-
-gulp.task('index', function () {
-	  return gulp.src('./src/main/webapp/index.html')
-	    .pipe(gulp.dest('d:/Profiles/vnhim/server/apache-tomcat-8.0.32/webapps/codeimpots-0.0.1-SNAPSHOT'));
-	});
-
-gulp.task('script', function () {
-	  return gulp.src('./src/main/webapp/script/*.js')
-	    .pipe(gulp.dest('d:/Profiles/vnhim/server/apache-tomcat-8.0.32/webapps/codeimpots-0.0.1-SNAPSHOT/script'));
-	});
-
  
 gulp.task('sass:watch', function () {
   gulp.watch('./assets/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('index:watch', function () {
-	  gulp.watch('./src/main/webapp/index.html', ['index']);
-	});
-gulp.task('script:watch', function () {
-	  gulp.watch('./src/main/webapp/script/*.js', ['script']);
-	});
-
-gulp.task('default', ['sass', 'index', 'sass:watch', 'index:watch', 'script:watch'])
+gulp.task('default', ['sass', 'sass:watch'])
